99a1cfc4d86863a8ab536f9276b8238092abac8c
Tzumx <>
2022-08-08 23:14:34 +0300

    drop token model

--- models/users.py
+++ models/users.py
@@ -18,20 +18,3 @@ users_table = sqlalchemy.Table(
         nullable=False,
     ),
 )
-
-
-tokens_table = sqlalchemy.Table(
-    "tokens",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column(
-        "token",
-        UUID(as_uuid=False),
-        server_default=sqlalchemy.text("uuid_generate_v4()"),
-        unique=True,
-        nullable=False,
-        index=True,
-    ),
-    sqlalchemy.Column("expires", sqlalchemy.DateTime()),
-    sqlalchemy.Column("user_id", sqlalchemy.ForeignKey("users.id")),
-)
