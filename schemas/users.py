1ba08eb4fac53365e5a9b60ca7d0d977b6f5d9c3
Tzumx <z.m.dev@outlook.com>
2022-08-10 10:21:01 +0300

    Add simple authorization (#5)
    * Add simple authorization
    
    * Change authenticated logic (remove token storage)
    
    * drop token model
    
    Co-authored-by: Tzumx <>

---
+++ schemas/users.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel, EmailStr, Field, validator
+from uuid import UUID
+
+
+class UserCreate(BaseModel):
+    """ Check sign-up request """
+
+    email: EmailStr
+    name: str
+    password: str
+
+
+class UserOut(BaseModel):
+    """ User answer """
+
+    id: int
+    email: str
+    name: str
+    # token: dict
+
+
+class SystemUser(UserOut):
+    """ Auth User check """
+
+    hashed_password: str
+
+
+class UserBase(BaseModel):
+    """ User information answer """
+
+    id: int
+    email: EmailStr
+    name: str
+
+
+class TokenBase(BaseModel):
+    """ Token check """
+
+    token: UUID = Field(..., alias="access_token")
+    expires: datetime
+    token_type: Optional[str] = "bearer"
+
+    class Config:
+        allow_population_by_field_name = True
+
+    @validator("token")
+    def hexlify_token(cls, value):
+        """ Convert UUID """
+
+        return value.hex
+
+
+class User(UserBase):
+    """ User information """
+
+    token: TokenBase = {}
+    is_active: bool
+
+
+class TokenSchema(BaseModel):
+    """ Token schema """
+
+    access_token: str
+    refresh_token: str
+
+
+class TokenPayload(BaseModel):
+    """ Token check auth """
+
+    sub: str = None
+    exp: int = None
